ff527f8147b47bf0974ddf0e615bb7278b2e241c
luwen <476314932@qq.com>
2021-12-11 17:00:34 +0800

    初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TabViewComponent } from './tab-view/tab-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabViewComponent
   ],
   imports: [
     BrowserModule
